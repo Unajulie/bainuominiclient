6a23d0737fd64aea54204579dbd4f22137508325
wuqiwei <1058633117@qq.com>
2021-09-20 12:56:15 +0800

    modify

---
+++ pages/report_epiliver/report_liverstatus.js
@@ -0,0 +1,122 @@
+// pages/report_epiage/report_epistatus.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+  showpdf: function (e) {
+    let phone = e.currentTarget.dataset.phone
+    let barcode = e.currentTarget.dataset.barcode
+    wx.request({
+      url: "https://bainuo.beijingepidial.com/client/epiage/ckreport",
+      header: {
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
+      method: "POST",
+      data: {
+        "barcode": barcode,
+        "phone": phone
+      },
+      // data: {"sampleid": 1121032800079},
+      complete: function (res) {
+        console.info(res.data)
+        if (res.data.pdf) {
+          let url = "../pdf/epiagepdf?pdfname=" + res.data.pdf
+          wx.navigateTo({
+            url: url
+          })
+        } else {
+          wx.showModal({
+            title: '提示',
+            content: "PDF努力生成中,请耐心等待"
+          })
+        }
+
+
+      },
+      fail: function (res) {}
+    })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    let oThis = this
+    wx.getStorage({
+      key: 'sessionuser',
+      success: function (res) {
+        //console.info(options.status )
+        let data = {}
+        data.sampleid = options.sampleid
+        data.phone = options.phone
+        console.info(data)
+        wx.request({
+          url: "https://bainuo.beijingepidial.com/client/liver/ckstatus",
+          header: { "Content-Type": "application/x-www-form-urlencoded"},
+          method: "POST",
+          data: data,
+          // data: {"sampleid": 1121032800079},
+          complete: function (res) {
+            oThis.setData({
+              status: res.data.status,
+              sampleid: res.data.sampleid
+            })
+          }
+        })
+      },
+      fail: function (res) {}
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
--- pages/report_epiliver/userform.js
+++ pages/report_epiliver/userform.js
@@ -45,67 +45,81 @@ Page({
     },
     //绑定输入的样本编码
     bininput_sampleid: function (e) {
-        this.setData({identity: e.detail.value})
+        this.setData({
+            identity: e.detail.value
+        })
         console.info(e.detail.value)
     },
-    btnckreport:function(e){
-        let data={}
-        data.idCard=this.data.identity
-        data.sex=0
-        data.tel=this.data.phone
-        data.sampleid=this.data.sampleid
-        data.username=this.data.username
-        data.created=new Date().toLocaleDateString()
-        console.info(data)
-        wx.request({
-            url: "https://bainuo.beijingepidial.com/client/liver/saveuser",
-            header: {"Content-Type": "application/x-www-form-urlencoded"},
-            method: "POST",
-            data: data,
-            // data: {"sampleid": 1121032800079},
-            complete: function (res) {
-              console.info(res.data)
-              //如果是空字符串
-            //   if(res.data.status=="success"){wx.showModal({title: '提示',content:"库存查无该条码，请联系客服"})}
-            //   else{
-            //     wx.navigateTo({url: 'userform?sampleid='+oThis.data.sampleid})
-            //   }
+    btnckreport: function (e) {
+        let oThis = this
+        let formdata = {}
+        formdata.idCard = this.data.identity
+        formdata.sex = 0
+        formdata.tel = this.data.phone
+        formdata.sampleid = this.data.sampleid
+        formdata.username = this.data.username
+        formdata.created = new Date().toLocaleDateString()
+        console.info(formdata)
+        wx.getStorage({
+            key: 'sessionuser',
+            success: function (sessionuser) {
+                wx.request({
+                    url: "https://bainuo.beijingepidial.com/client/liver/saveuser",
+                    header: {"Content-Type": "application/x-www-form-urlencoded"},
+                    method: "POST",
+                    data: formdata,
+                    // data: {"sampleid": 1121032800079},
+                    complete: function (res) {
+                        let url = '../report_epiliver/report_liverstatus?sampleid=' + oThis.data.sampleid + "&phone=" + sessionuser.data.phone+"&username"+oThis.data.username
+                        console.info(url)
+                        wx.navigateTo({
+                            url: url
+                        })
+                    }
+                })
             },
-          fail:function(res){
-          }})
+            fail: function (res) {
+                wx.showModal({title: '提示',content:"用户登录状态失效，请重新登录"})
+            }
+        })
     },
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
-        let sampleid=options.sampleid
+        let sampleid = options.sampleid
         let oThis = this
         wx.getStorage({
             key: 'sessionuser',
             success: function (res) {
-                oThis.setData({phone: res.data.phone,sampleid:sampleid})
-                let data={}
-                data.sampleid=sampleid
-                data.tel=res.data.phone
+                oThis.setData({
+                    phone: res.data.phone,
+                    sampleid: sampleid
+                })
+                let data = {}
+                data.sampleid = sampleid
+                data.tel = res.data.phone
                 console.info("xxx")
                 wx.request({
                     url: "https://bainuo.beijingepidial.com/client/liver/finduser",
-                    header: {"Content-Type": "application/x-www-form-urlencoded"},
+                    header: {
+                        "Content-Type": "application/x-www-form-urlencoded"
+                    },
                     method: "POST",
                     data: data,
                     // data: {"sampleid": 1121032800079},
                     complete: function (res) {
-                      oThis.setData({identity:res.data.idCard,username:res.data.username})  
-                      console.info(res.data)
-                      //如果是空字符串
-                    //   if(res.data.status=="success"){wx.showModal({title: '提示',content:"库存查无该条码，请联系客服"})}
-                    //   else{
-                    //     wx.navigateTo({url: 'userform?sampleid='+oThis.data.sampleid})
-                    //   }
+                        oThis.setData({
+                            identity: res.data.idCard,
+                            username: res.data.username
+                        })
+                        console.info(res.data)  
                     },
-                  fail:function(res){
-                  }})
-                
+                    fail: function (res) {
+                        wx.showModal({title: '提示',content:"用户登录状态失效，请重新登录"})
+                    }
+                })
+
             }
         })
     },
