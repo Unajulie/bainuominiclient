3e32e8a091a90d6a5d0bfc608a0b1b8a780da193
julie.una <771635995@qq.com>
2022-03-25 09:43:19 +0800

    julie

--- pages/report_generic/mergelist.js
+++ pages/report_generic/mergelist.js
@@ -4,7 +4,7 @@ Page({
      * 页面的初始数据
      */
     data: {
-
+        endate: new Date().toLocaleDateString().replace(new RegExp('/', 'g'), '-')
     },
     // 泛癌客户申请检测
     Mergeinfo: function (e) {
@@ -17,13 +17,63 @@ Page({
             },
         })
     },
+    // 选择开始时间
+    selStartdate: function (e) {
+        this.setData({
+            startdate: e.detail.value
+        })
+    },
+    // 选择结束时间
+    selTilldate: function (e) {
+        this.setData({
+            tilldate: e.detail.value
+        })
+    },
+    search: function (e) {
+        let that = this
+        let startime = that.data.startdate
+        let totime = that.data.tilldate
+        if (totime < startime) {
+            wx.showToast({
+                title: '时间段无效',
+                icon: 'error',
+                duration: 2000
+            })
+            this.setData({
+                startdate: "",
+                tilldate: ""
+            })
+        } else {
+            wx.getStorage({
+                key: 'sessionuser',
+                success: function (sessionuser) {
+                    console.info(sessionuser)
+                    let data = {}
+                    data.startdate = that.data.startdate
+                    data.tilldate = that.data.tilldate
+                    data.stafftel=sessionuser.data.phone
+                    wx.request({
+                        url: "https://bainuo.beijingepidial.com/client/generic/mergedlist/search",
+                        header: {
+                            "Content-Type": "application/x-www-form-urlencoded"
+                        },
+                        method: "POST",
+                        data: data,
+                        complete: function (res) {
+                            console.info(res)
+                        }
+                    })
+                },
+                fail: {}
+            })
+        }
+    },
     // 工作人员泛癌已绑定列表查看已绑定信息
     getMergedinfo: function (e) {
-        let that=this
-        console.info(e)
-        let data={}
-        data.sampleid=e.currentTarget.dataset.sampleid
-        data.username=e.currentTarget.dataset.username
+        let that = this
+        let data = {}
+        data.sampleid = e.currentTarget.dataset.sampleid
+        data.username = e.currentTarget.dataset.username
         console.info(data)
         wx.request({
             url: "https://bainuo.beijingepidial.com/client/generic/mergedinfo",
@@ -33,7 +83,7 @@ Page({
             method: "POST",
             data: data,
             complete: function (res) {
-                let url = '../report_generic/staffmergeinfo?sampleid=' + res.data.sampleid + "&username=" + res.data.username+"&disable="+res.data.disable
+                let url = '../report_generic/staffmergeinfo?sampleid=' + res.data.sampleid + "&username=" + res.data.username + "&disable=" + res.data.disable
                 console.info(url)
                 wx.navigateTo({
                     url: url
